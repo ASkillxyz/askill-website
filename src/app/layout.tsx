0fc894fc4dc5af35f023a8aadc235cc318c1ace5
Difeng Jiang <difeng@192.168.0.148>
2026-03-21 11:58:50 +0800

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.png', type: 'image/png', sizes: 'any' },
-      { url: '/favicon.svg', type: 'image/svg+xml' },
     ],
     shortcut: '/favicon.png',
     apple:    '/favicon.png',
